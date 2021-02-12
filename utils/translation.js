ec57019938a76af2e6a33cbe2930b1e479b29c94
Fernando <fernandopach2017@gmail.com>
2021-02-12 19:28:06 -0300

    Update translation system

--- utils/translation.js
+++ utils/translation.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const properties = require("properties");
 
 var options = {
-  sections: true,
   namespaces: true,
   variables: true,
   comments: "#",
@@ -12,26 +11,41 @@ var options = {
 
 module.exports = {
   /**
-   * Load translation bundle
-   * @param {String} languageCode - Language code
-   * @returns {Object}
-   */
-  loadBundle: function (languageCode = "en-us") {
-    if (!fs.existsSync(`bundles/${languageCode}.properties`))
-      languageCode = "en-us";
-    
-    let bundleData = fs.readFileSync(`bundles/${languageCode}.properties`, {
-      encoding: "utf8",
-    });
-
-    return properties.parse(bundleData, options);
-  },
-
-  /**
    * Get the language code for all existing translations
    * @returns {String[]} Language code for all existing translations
    */
-  getAvailableBundles: function () {
-    return fs.readdirSync("bundles").map(lc => { return lc.split(".")[0] });
+  getBundles: function () {
+    return fs.readdirSync("bundles").map(b => { return b.split(".")[0] });
+  },
+
+  /**
+   * Load translation bundle
+   * @param {String} [languageCode="en-US"] - Language code
+   * @returns {BundleData}
+   */
+  loadBundle: function (languageCode = "en-US") {
+    var bundleData;
+    if(!module.exports.getBundles().includes(languageCode)) {
+      bundleData = fs.readFileSync(`bundles/src/src.properties`, { encoding: "utf8" });
+    } else {
+      bundleData = fs.readFileSync(`bundles/${languageCode}.properties`, { encoding: "utf8" });
+    }
+
+    return properties.parse(bundleData, options);
   }
 };
+
+/**
+ * @typedef {Object} BundleData
+ * @property {Object} commons - Commons sentences used in several files
+ * @property {Object} commons.bot
+ * @property {String} commons.bot.noperm
+ * @property {String} commons.bot.needperm
+ * @property {Object} commons.user
+ * @property {String} commons.user.noperm
+ * @property {String} commons.user.needperm
+ * @property {Object} commons.error
+ * @property {String} commons.error.title
+ * @property {String} commons.error.desc
+ * 
+ */
