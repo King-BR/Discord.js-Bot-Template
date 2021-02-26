627ac13ebf0490041c4a5ace2379ac6cda63c7f6
Fernando <fernandopach2017@gmail.com>
2021-02-26 18:16:19 -0300

    Update message event

--- events/Message/message.js
+++ events/Message/message.js
@@ -56,7 +56,8 @@ module.exports = function (client, message) {
       return message.channel.send(
         bundle.events.message.mention
           .replace(/\{0\}/g, message.author.toString())
-          .replace(/\{1\}/g, `${prefix}help`)
+          .replace(/\{1\}/g, prefix)
+          .replace(/\{2\}/g, `${prefix}help`)
       );
 
     let commandfile =
