c86ae8c43c54dedba202d1ef96b5ff6ccb885516
Fernando <fernandopach2017@gmail.com>
2021-03-03 19:22:56 -0300

    Update help command

--- commands/Info/help.js
+++ commands/Info/help.js
@@ -101,6 +101,9 @@ module.exports = {
             { name: bundle.commands.help.cmdHelp.perm, value: perms, inline: true},
             { name: bundle.commands.help.cmdHelp.guildOnly, value: cmd.config.guildOnly ? bundle.commons.yes : bundle.commons.no, inline: true },
             { name: bundle.commands.help.cmdHelp.dmOnly, value: cmd.config.dmOnly ? bundle.commons.yes : bundle.commons.no, inline: true },
+            { name: "\u200b", value: "\u200b", inline: true },
+            { name: bundle.commands.help.cmdHelp.ownerOnly, value: cmd.config.ownerOnly ? bundle.commons.yes : bundle.commons.no, inline: true },
+            { name: bundle.commands.help.cmdHelp.staffOnly, value: cmd.config.staffOnly ? bundle.commons.yes : bundle.commons.no, inline: true },
             { name: bundle.commands.help.cmdHelp.devOnly, value: cmd.config.devOnly ? bundle.commons.yes : bundle.commons.no, inline: true }
           ])
           .setColor("RANDOM")
@@ -119,7 +122,8 @@ module.exports = {
     desc: "See the list of commands or get extended help about one command", // Command description
     usage: "[command name]", // Usage parameters, null if there are no parameters
     permissions: [], // Permission(s) needed to use the command
-    guildOnly: true, // Determines whether the command can only be used on a server
+    guildOnly: false, // Determines whether the command can only be used on a server
+    staffOnly: false, // Determines whether the command can only be used by the server staff
     dmOnly: false, // Determines whether the command can only be used on DM
     ownerOnly: false, // Determines whether the command can only be used by the server owner
     devOnly: false, // Determines whether the command can only be used by the dev team
