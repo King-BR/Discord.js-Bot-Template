84878b400044a61927a329858c00e33875a0376d
Fernando <fernandopach2017@gmail.com>
2021-03-12 17:52:51 -0300

    Rename bundles folder to languages

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ const moduleAlias = require("module-alias");
 moduleAlias.addAliases({
   "@utils": __dirname + "/src/utils",
   "@structures": __dirname + "/src/structures",
-  "@bundles": __dirname + "src/bundles",
+  "@languages": __dirname + "/src/languages",
 });
