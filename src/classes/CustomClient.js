42263e734b72f31ab566e286b6d9cf9ce63897fb
Fernando <fernandopach2017@gmail.com>
2021-03-19 20:27:02 -0300

    update CustomClient

--- src/classes/CustomClient.js
+++ src/classes/CustomClient.js
@@ -1,12 +1,14 @@
-const { Client, Intents } = require("discord.js");
-const {
-  privilegedIntents,
-  partials,
-  translation,
-} = require("../../config.json");
+// prettier-ignore
+const { privilegedIntents, partials, translation } = require("../../config.json");
+const Discord = require("discord.js");
 const { Player } = require("discord-player");
 
-class CustomClient extends Client {
+/**
+ * Custom client made for the template
+ * @class
+ * @augments Discord.Client
+ */
+class CustomClient extends Discord.Client {
   /**
    * @param {Boolean} dev
    * @constructor
@@ -21,13 +23,16 @@ class CustomClient extends Client {
       messageSweepInterval: 300,
       messageEditHistoryMaxSize: 2,
       ws: {
-        intents: privilegedIntents ? Intents.ALL : Intents.NON_PRIVILEGED,
+        intents: privilegedIntents
+          ? Discord.Intents.ALL
+          : Discord.Intents.NON_PRIVILEGED,
       },
     });
 
     this.dev = dev || false;
     this.console = console; // TODO implement logger
-    this.utils = require("@utils/utils.js");
+    this.utils = require("../utils/utils.js");
+    this.translation = translation;
     this.player = new Player(this, {
       leaveOnEnd: true,
       leaveOnEndCooldown: 60,
@@ -40,8 +45,36 @@ class CustomClient extends Client {
     });
   }
 
+  /**
+   * Logs the client in, establishing a websocket connection to Discord.
+   * @returns {Promise<String>} Token of the account used
+   */
   login() {
     // prettier-ignore
     return super.login(this.dev ? process.env.DISCORD_TOKEN_DEV : process.env.DISCORD_TOKEN);
   }
+
+  /**
+   * Get an emoji by ID
+   * @param {Number} ID
+   * @returns {Discord.GuildEmoji}
+   */
+  getEmojiByID(ID) {
+    if (ID == null || isNaN(ID)) return null;
+    return this.emojis.fetch(ID);
+  }
+
+  /**
+   * Get an emoji by name
+   * @param {String} name
+   * @returns {Discord.GuildEmoji}
+   */
+  getEmojiByName(name) {
+    if (name == null) return null;
+    return this.emojis.cache.find((e) => {
+      return e.name == name;
+    });
+  }
 }
+
+module.exports = CustomClient;
